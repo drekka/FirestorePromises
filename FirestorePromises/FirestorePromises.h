19c06fe6394ec223a80a691d93630b2a440572a4
Derek Clarkson <d4rkf1br3@gmail.com>
2018-01-13 15:13:14 +1100

    Initial Commit

---
+++ FirestorePromises/FirestorePromises.h
@@ -0,0 +1,19 @@
+//
+//  FirestorePromises.h
+//  FirestorePromises
+//
+//  Created by Derek Clarkson on 13/1/18.
+//  Copyright © 2018 Derek Clarkson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirestorePromises.
+FOUNDATION_EXPORT double FirestorePromisesVersionNumber;
+
+//! Project version string for FirestorePromises.
+FOUNDATION_EXPORT const unsigned char FirestorePromisesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirestorePromises/PublicHeader.h>
+
+
